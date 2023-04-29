2b085e97eee98a9a79bd4885d45d03e1f004a28b
adir-kandel <adir@eureka.security>
2023-04-29 20:17:33 +0300

    change state structure

---
+++ src/core/components/Counter.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="custom-number-input h-8 w-24 flex flex-col">
+    <label v-if="props.label" for="custom-input-number" class="w-full text-gray-700 text-sm mb-1 font-semibold">
+      {{ props.label }}
+    </label>
+    <div class="flex flex-row flex-1 w-full rounded-lg relative bg-transparent">
+      <button @click="decrement"
+              class="bg-gray-100 text-gray-600 hover:bg-gray-200 h-full w-12 rounded-r cursor-pointer outline-none">
+        <span class="m-auto text-lg font-thin">−</span>
+      </button>
+      <input type="number"
+             class="appearance-none text-center w-full bg-gray-100 text-md flex items-center text-gray-600 outline-none"
+             name="custom-input-number" v-model.number="value" />
+      <button @click="increment"
+              class="bg-gray-100 text-gray-600 hover:bg-gray-200 h-full w-12 rounded-l cursor-pointer">
+        <span class="m-auto text-lg font-thin">+</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  label: { type: String, required: false },
+  modelValue: { type: Number, required: true },
+});
+const emit = defineEmits(['update:modelValue'])
+
+let value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
+
+const decrement = () => {
+  value.value -= 1
+}
+const increment = () => {
+  value.value += 1
+}
+</script>
+
+<style scoped>
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,28 +1,40 @@
 <template>
   <div class="2xl:w-1/3 md:w-2/5 mx-auto px-4 py-8">
-    <h1 class="text-3xl font-bold text-center mb-8">My Shopping List</h1>
+    <div class="flex items-center justify-between mb-8">
+      <h1 class="text-3xl font-bold">{{ activeList.name || 'רשימת קניות' }}</h1>
+
+      <button class="flex items-center gap-x-1 px-3 py-2 rounded-md bg-green-500 text-white text-sm hover:bg-green-600 focus:outline-none">
+        <IconAdd />
+        הוסף פריט
+      </button>
+    </div>
 
     <div class="flex flex-col gap-6">
-      <template v-for="category in store.categories" :key="category.uuid">
+      <template v-for="category in availableCategories" :key="category.uuid">
         <div class="flex flex-col">
           <div class="flex items-center justify-between border-b border-gray pb-3 mb-3">
             <h2 class="text-xl font-bold">{{ category.name }}</h2>
-            <button type="submit" class="px-3 py-2 rounded-md bg-green-500 text-white text-sm hover:bg-green-600 focus:outline-none">
-              Add
+            <button @click="toggleCategory(category.uuid)">
+              <IconChevronDown v-if="closedCategories.includes(category.uuid)" />
+              <IconChevronUp v-else />
             </button>
           </div>
 
-          <ul class="flex flex-col gap-y-3">
-            <template v-for="item in store.getItemsByCategory(category.uuid)" :key="item.uuid">
-              <li class="bg-white shadow-md rounded-md p-4 flex items-center gap-x-2 py-3 text-sm font-medium text-gray-800 dark:text-white">
-                <div class="flex items-center gap-x-3 w-full">
+          <ul class="flex flex-col gap-y-4" :class="{hidden: closedCategories.includes(category.uuid)}">
+            <template v-for="item in getItemsByCategory(category.uuid)" :key="item.uuid">
+              <li class="shadow rounded-md overflow-hidden flex items-center h-[3.5rem]">
+                <button class="h-full w-10 text-center bg-green-500 text-white">
+                  <IconCheck class="mx-auto" />
+                </button>
+                <div class="flex items-center px-4 gap-x-3 bg-white text-sm font-medium text-gray-800 flex-1">
                   <img :src="`https://loremflickr.com/200/200/${item.name}`" :alt="item.name" class="w-10 h-10 rounded-md" />
                   <span class="ml-2 font-semibold text-gray-700"
                         :class="{ 'line-through': item.isChecked }">{{ item.name }}</span>
-                  <span class="inline-flex items-center py-1 px-2 rounded-full text-xs font-medium bg-blue-500 text-white mr-auto">
-                    {{ item.quantity }}
-                  </span>
+                  <Counter v-model="item.quantity" class="mr-auto" />
                 </div>
+                <button class="h-full w-10 text-center bg-red-500 text-white">
+                  <IconClose class="mx-auto" />
+                </button>
               </li>
             </template>
           </ul>
@@ -33,12 +45,40 @@
 </template>
 
 <script lang="ts" setup>
-import { useShoppingListStore } from "../stores/shopping-list";
+import IconAdd from '~icons/carbon/add'
+import IconCheck from '~icons/carbon/checkmark'
+import IconClose from '~icons/carbon/close'
+import IconChevronDown from '~icons/carbon/chevron-down'
+import IconChevronUp from '~icons/carbon/chevron-up'
+import { Category, useShoppingListStore } from "../stores/shopping-list";
+import uniqBy from 'lodash/uniqBy';
+import Counter from "../core/components/Counter.vue";
+import { reactive } from "vue";
 
 const store = useShoppingListStore();
 
-const addItem = () => {
-  // store.addItem(category.id)
+const activeList = store.activeList
+
+const items = reactive(activeList.items.map(item => ({
+  ...item,
+  ...store.products.find(product => product.uuid === item.productId)
+})))
+
+const availableCategories: Category[] = uniqBy(items.map(item => ({ ...store.categories.find(category => category.uuid === item.categoryId)!})), 'uuid');
+
+const getItemsByCategory = (categoryId: Category['uuid']) => {
+  return items.filter(item => item.categoryId === categoryId)
+}
+
+const closedCategories: string[] = reactive([])
+
+const toggleCategory = (categoryId: string) => {
+  const categoryIdx = closedCategories.findIndex(category => category === categoryId)
+  if (categoryIdx >=0) {
+    closedCategories.splice(categoryIdx, 1)
+  } else {
+    closedCategories.push(categoryId)
+  }
 }
 </script>
 
